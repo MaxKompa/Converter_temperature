3297ec7772efbf0e3ef88b571453d054c7d3cecf
MaxKompa <max.kompa645@gmail.com>
2024-06-12 17:37:00 +0200

    Add fahrenheit to celsius convert

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,5 +5,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Converter Application");
+
+        double fahrenheit = 98.6;
+        double celsius = fahrenheitToCelsius(fahrenheit);
+
+        System.out.println("Result is " + celsius + " celsius.");
+
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
     }
 }
